08628c85cbbf2f185e95c3900b3effb5c773541f
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-05-01 20:52:04 +0530

    Add files via upload

---
+++ Patterns/02_Pattern(odd_num_in_columns).py
@@ -0,0 +1,15 @@
+#steps to solve the pattern
+# 1. for the outer loop, count the number of line or rows
+# 2. for the inner loop, focus on the colums and connect them somehow to the rows
+# 3. print them "*" inside the inner for loop
+# 4. observe symmettry [optional]
+
+
+num = int(input("enter the row: "))
+k = 1
+for i in range(1, num+1):
+    for j in range(1, k+1):
+        print("x", end=" ")
+    k = k + 2
+    print()
+ 
