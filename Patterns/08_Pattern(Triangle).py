3ae759c32b11666fc6a96af6450f7a7d92919b50
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-05-18 22:44:55 +0530

    Add files via upload

---
+++ Patterns/08_Pattern(Triangle).py
@@ -0,0 +1,16 @@
+def pattern(n):
+    for row in range(1, n+1):
+        for col in range(n-row):
+            print(" ", end="")
+        for col in range(2*row-1): 
+            print("*", end="")
+        print()
+
+def main():
+    n = 5
+    pattern(n)
+
+if __name__=="__main__":
+    main()
+
+
